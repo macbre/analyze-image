e56e64b5325a7c6358b27b4ad692fadd831860e1
macbre <maciej.brencz@gmail.com>
2025-03-05 09:35:59 +0000

    WIP

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,9 @@
-const { describe, it } = require("@jest/globals");
+import { describe, it } from "@jest/globals";
 
-const analyzeImage = require('../.'),
-    assert = require('assert'),
-    fs = require('fs').promises,
-    path = require('path');
+import analyzeImage, { EXIT_IMAGE_PASSED_IS_INVALID, EXIT_EMPTY_IMAGE } from '../lib/index.js';
+import { strictEqual, ok } from 'assert';
+import { promises as fs } from 'fs';
+import { resolve } from 'path';
 
 
 describe('Image parameter', () => {
@@ -12,31 +12,31 @@ describe('Image parameter', () => {
             try {
                 await analyzeImage(42);
             } catch(error) {
-                assert.strictEqual(error.code, analyzeImage.EXIT_IMAGE_PASSED_IS_INVALID, 'Error code 253 is returned');
+                strictEqual(error.code, EXIT_IMAGE_PASSED_IS_INVALID, 'Error code 253 is returned');
             }
 
             try {
                 await analyzeImage(true);
             } catch(error) {
-                assert.strictEqual(error.code, analyzeImage.EXIT_IMAGE_PASSED_IS_INVALID, 'Error code 253 is returned');
+                strictEqual(error.code, EXIT_IMAGE_PASSED_IS_INVALID, 'Error code 253 is returned');
             }
 
             try {
                 await analyzeImage({});
             } catch(error) {
-                assert.strictEqual(error.code, analyzeImage.EXIT_IMAGE_PASSED_IS_INVALID, 'Error code 253 is returned');
+                strictEqual(error.code, EXIT_IMAGE_PASSED_IS_INVALID, 'Error code 253 is returned');
             }
 
             try {
                 await analyzeImage(undefined);
             } catch(error) {
-                assert.strictEqual(error.code, analyzeImage.EXIT_IMAGE_PASSED_IS_INVALID, 'Error code 253 is returned');
+                strictEqual(error.code, EXIT_IMAGE_PASSED_IS_INVALID, 'Error code 253 is returned');
             }
         });
 
         it('should fail loading improper format in a Promise way', async () => {
             analyzeImage(42).catch(function(error) {
-                assert.strictEqual(error.code, analyzeImage.EXIT_IMAGE_PASSED_IS_INVALID, 'Error code 253 is returned');
+                strictEqual(error.code, EXIT_IMAGE_PASSED_IS_INVALID, 'Error code 253 is returned');
             });
         });
     });
@@ -46,7 +46,7 @@ describe('Image parameter', () => {
             try {
                 await analyzeImage('');
             } catch(error) {
-                assert.strictEqual(error.code, analyzeImage.EXIT_EMPTY_IMAGE, 'Error code 252 is returned');
+                strictEqual(error.code, EXIT_EMPTY_IMAGE, 'Error code 252 is returned');
             }
         });
 
@@ -54,47 +54,47 @@ describe('Image parameter', () => {
             try {
                 await analyzeImage(Buffer.from([]));
             } catch(error) {
-                assert.strictEqual(error.code, analyzeImage.EXIT_EMPTY_IMAGE, 'Error code 252 is returned');
+                strictEqual(error.code, EXIT_EMPTY_IMAGE, 'Error code 252 is returned');
             }
         });
     });
 
     describe('Image as a string', () => {
         it('should load an SVG image', async () => {
-            const image = await fs.readFile(path.resolve(__dirname, './images/svg-image.svg'));
+            const image = await fs.readFile(resolve(__dirname, './images/svg-image.svg'));
             const res = await analyzeImage(image.toString());
-            assert.strictEqual(res.stats.mimeType, 'image/svg+xml', 'Mime type is correct');
-            assert.strictEqual(res.stats.format, 'svg', 'Format is correct');
+            strictEqual(res.stats.mimeType, 'image/svg+xml', 'Mime type is correct');
+            strictEqual(res.stats.format, 'svg', 'Format is correct');
         });
 
         it('should load a base64 encoded JPEG image', async () => {
-            const image = await fs.readFile(path.resolve(__dirname, './images/jpeg-image.jpg'), {encoding: 'base64'});
+            const image = await fs.readFile(resolve(__dirname, './images/jpeg-image.jpg'), {encoding: 'base64'});
             const res = await analyzeImage(image);
-            assert.strictEqual(res.stats.mimeType, 'image/jpeg', 'Mime type is correct');
-            assert.strictEqual(res.stats.format, 'jpg', 'Format is correct');
+            strictEqual(res.stats.mimeType, 'image/jpeg', 'Mime type is correct');
+            strictEqual(res.stats.format, 'jpg', 'Format is correct');
         });
     });
 
     describe('Image as a buffer', () => {
         it('should load a PNG image', async () => {
-            const image = await fs.readFile(path.resolve(__dirname, './images/png-image.png'));
+            const image = await fs.readFile(resolve(__dirname, './images/png-image.png'));
             const res = await analyzeImage(image);
-            assert.strictEqual(res.stats.mimeType, 'image/png', 'Mime type is correct');
-            assert.strictEqual(res.stats.format, 'png', 'Format is correct');
+            strictEqual(res.stats.mimeType, 'image/png', 'Mime type is correct');
+            strictEqual(res.stats.format, 'png', 'Format is correct');
         });
 
         it('should load an SVG inside a buffer', async () => {
-            const image = await fs.readFile(path.resolve(__dirname, './images/svg-image.svg'));
+            const image = await fs.readFile(resolve(__dirname, './images/svg-image.svg'));
             const buffer = Buffer.from(image, 'base64');
             const res = await analyzeImage(buffer);
-            assert.strictEqual(res.stats.mimeType, 'image/svg+xml', 'Mime type is correct');
-            assert.strictEqual(res.stats.format, 'svg', 'Format is correct');
+            strictEqual(res.stats.mimeType, 'image/svg+xml', 'Mime type is correct');
+            strictEqual(res.stats.format, 'svg', 'Format is correct');
         });
     });
 
     describe('Output format', () => {
         it('should be respected', async () => {
-            const image = await fs.readFile(path.resolve(__dirname, './images/jpeg-image.jpg'));
+            const image = await fs.readFile(resolve(__dirname, './images/jpeg-image.jpg'));
             const res = await analyzeImage(image, {
                 displayWidth: 200,
                 displayHeight: 100,
@@ -104,63 +104,63 @@ describe('Image parameter', () => {
             }, {removeBuffersFromTransforms: false});
             
             // Stats
-            assert.strictEqual(res.stats.format, 'jpg');
-            assert.strictEqual(res.stats.mimeType, 'image/jpeg');
-            assert.strictEqual(res.stats.fileSize, image.length);
-            assert.strictEqual(res.stats.width, 285);
-            assert.strictEqual(res.stats.height, 427);
-            assert.strictEqual(res.stats.animated, false);
-            assert.strictEqual(res.stats.sizesAttribute, '5vw');
-            assert.strictEqual(res.stats.srcsetAttribute, 'image1.jpg 10w, image2.jpg 20w');
-            assert.strictEqual(res.stats.displayDensity.toFixed(4), '2.8475');
-            assert.strictEqual(res.stats.displayRatio.toFixed(4), '2.8475');
+            strictEqual(res.stats.format, 'jpg');
+            strictEqual(res.stats.mimeType, 'image/jpeg');
+            strictEqual(res.stats.fileSize, image.length);
+            strictEqual(res.stats.width, 285);
+            strictEqual(res.stats.height, 427);
+            strictEqual(res.stats.animated, false);
+            strictEqual(res.stats.sizesAttribute, '5vw');
+            strictEqual(res.stats.srcsetAttribute, 'image1.jpg 10w, image2.jpg 20w');
+            strictEqual(res.stats.displayDensity.toFixed(4), '2.8475');
+            strictEqual(res.stats.displayRatio.toFixed(4), '2.8475');
 
             // Transforms
-            assert.strictEqual(res.transforms.optimized.fileSize, image.length);
-            assert.ok(res.transforms.optimized.newFileSize < image.length);
-            assert.strictEqual(Buffer.isBuffer(res.transforms.optimized.body), true);
-            assert.strictEqual(res.transforms.optimized.body.length, res.transforms.optimized.newFileSize);
+            strictEqual(res.transforms.optimized.fileSize, image.length);
+            ok(res.transforms.optimized.newFileSize < image.length);
+            strictEqual(Buffer.isBuffer(res.transforms.optimized.body), true);
+            strictEqual(res.transforms.optimized.body.length, res.transforms.optimized.newFileSize);
 
-            assert.strictEqual(res.transforms.resized.fileSize, res.transforms.optimized.newFileSize);
-            assert.ok(res.transforms.resized.newFileSize < res.transforms.optimized.newFileSize);
-            assert.strictEqual(res.transforms.resized.naturalWidth, 285);
-            assert.strictEqual(res.transforms.resized.naturalHeight, 427);
-            assert.strictEqual(res.transforms.resized.newWidth, 200);
-            assert.strictEqual(res.transforms.resized.newHeight, 100);
-            assert.strictEqual(Buffer.isBuffer(res.transforms.resized.body), true);
-            assert.strictEqual(res.transforms.resized.body.length, res.transforms.resized.newFileSize);
+            strictEqual(res.transforms.resized.fileSize, res.transforms.optimized.newFileSize);
+            ok(res.transforms.resized.newFileSize < res.transforms.optimized.newFileSize);
+            strictEqual(res.transforms.resized.naturalWidth, 285);
+            strictEqual(res.transforms.resized.naturalHeight, 427);
+            strictEqual(res.transforms.resized.newWidth, 200);
+            strictEqual(res.transforms.resized.newHeight, 100);
+            strictEqual(Buffer.isBuffer(res.transforms.resized.body), true);
+            strictEqual(res.transforms.resized.body.length, res.transforms.resized.newFileSize);
 
-            assert.strictEqual(res.transforms.webpEncoded.fileSize, image.length);
-            assert.ok(res.transforms.webpEncoded.newFileSize < image.length);
-            assert.strictEqual(Buffer.isBuffer(res.transforms.webpEncoded.body), true);
-            assert.strictEqual(res.transforms.webpEncoded.body.length, res.transforms.webpEncoded.newFileSize);
+            strictEqual(res.transforms.webpEncoded.fileSize, image.length);
+            ok(res.transforms.webpEncoded.newFileSize < image.length);
+            strictEqual(Buffer.isBuffer(res.transforms.webpEncoded.body), true);
+            strictEqual(res.transforms.webpEncoded.body.length, res.transforms.webpEncoded.newFileSize);
 
-            assert.strictEqual(res.transforms.avifEncoded.fileSize, image.length);
-            assert.ok(res.transforms.avifEncoded.newFileSize < image.length);
-            assert.strictEqual(Buffer.isBuffer(res.transforms.avifEncoded.body), true);
-            assert.strictEqual(res.transforms.avifEncoded.body.length, res.transforms.avifEncoded.newFileSize);
+            strictEqual(res.transforms.avifEncoded.fileSize, image.length);
+            ok(res.transforms.avifEncoded.newFileSize < image.length);
+            strictEqual(Buffer.isBuffer(res.transforms.avifEncoded.body), true);
+            strictEqual(res.transforms.avifEncoded.body.length, res.transforms.avifEncoded.newFileSize);
 
             // Offenders
-            assert.strictEqual(res.offenders.imageNotOptimized.fileSize, image.length);
-            assert.ok(res.offenders.imageNotOptimized.newFileSize < image.length);
+            strictEqual(res.offenders.imageNotOptimized.fileSize, image.length);
+            ok(res.offenders.imageNotOptimized.newFileSize < image.length);
 
-            assert.strictEqual(res.offenders.imageScaledDown.fileSize, res.transforms.optimized.newFileSize);
-            assert.ok(res.offenders.imageScaledDown.newFileSize < res.transforms.optimized.newFileSize);
-            assert.strictEqual(res.offenders.imageScaledDown.naturalWidth, 285);
-            assert.strictEqual(res.offenders.imageScaledDown.naturalHeight, 427);
-            assert.strictEqual(res.offenders.imageScaledDown.newWidth, 200);
-            assert.strictEqual(res.offenders.imageScaledDown.newHeight, 100);
+            strictEqual(res.offenders.imageScaledDown.fileSize, res.transforms.optimized.newFileSize);
+            ok(res.offenders.imageScaledDown.newFileSize < res.transforms.optimized.newFileSize);
+            strictEqual(res.offenders.imageScaledDown.naturalWidth, 285);
+            strictEqual(res.offenders.imageScaledDown.naturalHeight, 427);
+            strictEqual(res.offenders.imageScaledDown.newWidth, 200);
+            strictEqual(res.offenders.imageScaledDown.newHeight, 100);
 
-            assert.strictEqual(res.offenders.imageOldFormat.fileSize, image.length);
-            assert.ok(res.offenders.imageOldFormat.newFileSize > 0);
-            assert.ok(res.offenders.imageOldFormat.webpSize > 0);
-            assert.ok(res.offenders.imageOldFormat.avifSize > 0);
+            strictEqual(res.offenders.imageOldFormat.fileSize, image.length);
+            ok(res.offenders.imageOldFormat.newFileSize > 0);
+            ok(res.offenders.imageOldFormat.webpSize > 0);
+            ok(res.offenders.imageOldFormat.avifSize > 0);
 
-            assert.strictEqual(res.offenders.imageWithIncorrectSizesParam.sizesAttribute, '5vw');
-            assert.strictEqual(res.offenders.imageWithIncorrectSizesParam.convertedInPx, 60);
-            assert.strictEqual(res.offenders.imageWithIncorrectSizesParam.displayWidth, 200);
+            strictEqual(res.offenders.imageWithIncorrectSizesParam.sizesAttribute, '5vw');
+            strictEqual(res.offenders.imageWithIncorrectSizesParam.convertedInPx, 60);
+            strictEqual(res.offenders.imageWithIncorrectSizesParam.displayWidth, 200);
 
-            assert.ok(!res.offenders.imageExcessiveDensity);
+            ok(!res.offenders.imageExcessiveDensity);
         });
     });
 });
--- test/reformat.test.js
+++ test/reformat.test.js
@@ -1,37 +1,37 @@
-const { describe, it } = require("@jest/globals");
+import { describe, it } from "@jest/globals";
 
-const ModulesRunner = require('../lib/modulesRunner'),
-    assert = require('assert'),
-    fs = require('fs').promises,
-    path = require('path')
+import { execModuleForTest } from '../lib/modulesRunner';
+import { ok, strictEqual } from 'assert';
+import { promises as fs } from 'fs';
+import { resolve } from 'path';
     sharp = require('sharp');
 
 
 describe('Reformat module', () => {
     it ('should not reformat an SVG', async () => {
-        const image = await fs.readFile(path.resolve(__dirname, './images/svg-image.svg'));
-        const result = await ModulesRunner.execModuleForTest('reformat', image, {}, {}, {stats: {
+        const image = await fs.readFile(resolve(__dirname, './images/svg-image.svg'));
+        const result = await execModuleForTest('reformat', image, {}, {}, {stats: {
             format: 'svg',
             fileSize: image.length
         }});
-        assert.ok(!result.transforms.reformat, 'Should not reformat image');
+        ok(!result.transforms.reformat, 'Should not reformat image');
     });
     it ('should not reformat an AVIF', async () => {
-        const image = await fs.readFile(path.resolve(__dirname, './images/avif-image.avif'));
-        const result = await ModulesRunner.execModuleForTest('reformat', image, {}, {}, {stats: {
+        const image = await fs.readFile(resolve(__dirname, './images/avif-image.avif'));
+        const result = await execModuleForTest('reformat', image, {}, {}, {stats: {
             format: 'avif',
             fileSize: image.length
         }});
-        assert.ok(!result.transforms.reformat, 'Should not reformat image');
+        ok(!result.transforms.reformat, 'Should not reformat image');
     });
     it ('should not reformat an animated WebP', async () => {
-        const image = await fs.readFile(path.resolve(__dirname, './images/animated.webp'));
-        const result = await ModulesRunner.execModuleForTest('reformat', image, {}, {}, {stats: {
+        const image = await fs.readFile(resolve(__dirname, './images/animated.webp'));
+        const result = await execModuleForTest('reformat', image, {}, {}, {stats: {
             format: 'webp',
             fileSize: image.length,
             animated: true
         }});
-        assert.ok(!result.transforms.reformat, 'Should not reformat image');
+        ok(!result.transforms.reformat, 'Should not reformat image');
     });
 
 
@@ -43,25 +43,25 @@ describe('Reformat module', () => {
 
     files.forEach(file => {
         it('should succesfully reformat a ' + file.format + ' to webp', async () => {
-            const image = await fs.readFile(path.resolve(__dirname, './images/', file.name));
-            const result = await ModulesRunner.execModuleForTest('reformat', image, {}, {removeBuffersFromTransforms: false}, {stats: {
+            const image = await fs.readFile(resolve(__dirname, './images/', file.name));
+            const result = await execModuleForTest('reformat', image, {}, {removeBuffersFromTransforms: false}, {stats: {
                 format: file.format,
                 fileSize: image.length,
                 animated: file.animated
             }});
 
-            assert.ok(result.transforms.webpEncoded.currentFormat, files.format);
-            assert.ok(result.transforms.webpEncoded.newFileSize > 0, 'New file weight is provided');
-            assert.strictEqual(result.transforms.webpEncoded.body.length, result.transforms.webpEncoded.newFileSize);
-            assert.ok(result.transforms.webpEncoded.fileSize > 0);
+            ok(result.transforms.webpEncoded.currentFormat, files.format);
+            ok(result.transforms.webpEncoded.newFileSize > 0, 'New file weight is provided');
+            strictEqual(result.transforms.webpEncoded.body.length, result.transforms.webpEncoded.newFileSize);
+            ok(result.transforms.webpEncoded.fileSize > 0);
 
-            const newContentType = await ModulesRunner.execModuleForTest('contentType', result.transforms.webpEncoded.body);
-            assert.strictEqual(newContentType.stats.format, 'webp', 'Content type is webp');
+            const newContentType = await execModuleForTest('contentType', result.transforms.webpEncoded.body);
+            strictEqual(newContentType.stats.format, 'webp', 'Content type is webp');
 
-            assert.ok(result.offenders.imageOldFormat.currentFormat, files.format);
-            assert.strictEqual(result.offenders.imageOldFormat.fileSize, image.length);
-            assert.ok(result.offenders.imageOldFormat.newFileSize > 0);
-            assert.ok(result.offenders.imageOldFormat.newFileSize < result.offenders.imageOldFormat.fileSize);
+            ok(result.offenders.imageOldFormat.currentFormat, files.format);
+            strictEqual(result.offenders.imageOldFormat.fileSize, image.length);
+            ok(result.offenders.imageOldFormat.newFileSize > 0);
+            ok(result.offenders.imageOldFormat.newFileSize < result.offenders.imageOldFormat.fileSize);
         });
     });
 
@@ -73,25 +73,25 @@ describe('Reformat module', () => {
 
     files.forEach(file => {
         it('should succesfully reformat a ' + file.format + ' to avif', async () => {
-            const image = await fs.readFile(path.resolve(__dirname, './images/', file.name));
-            const result = await ModulesRunner.execModuleForTest('reformat', image, {}, {removeBuffersFromTransforms: false}, {stats: {
+            const image = await fs.readFile(resolve(__dirname, './images/', file.name));
+            const result = await execModuleForTest('reformat', image, {}, {removeBuffersFromTransforms: false}, {stats: {
                 format: file.format,
                 fileSize: image.length,
                 animated: file.animated
             }});
 
-            assert.ok(result.transforms.avifEncoded.currentFormat, files.format);
-            assert.ok(result.transforms.avifEncoded.newFileSize > 0, 'New file weight is provided');
-            assert.strictEqual(result.transforms.avifEncoded.body.length, result.transforms.avifEncoded.newFileSize);
-            assert.ok(result.transforms.avifEncoded.fileSize > 0);
+            ok(result.transforms.avifEncoded.currentFormat, files.format);
+            ok(result.transforms.avifEncoded.newFileSize > 0, 'New file weight is provided');
+            strictEqual(result.transforms.avifEncoded.body.length, result.transforms.avifEncoded.newFileSize);
+            ok(result.transforms.avifEncoded.fileSize > 0);
 
-            const newContentType = await ModulesRunner.execModuleForTest('contentType', result.transforms.avifEncoded.body);
-            assert.strictEqual(newContentType.stats.format, 'avif', 'Content type is webp');
+            const newContentType = await execModuleForTest('contentType', result.transforms.avifEncoded.body);
+            strictEqual(newContentType.stats.format, 'avif', 'Content type is webp');
 
-            assert.ok(result.offenders.imageOldFormat.currentFormat, files.format);
-            assert.strictEqual(result.offenders.imageOldFormat.fileSize, image.length);
-            assert.ok(result.offenders.imageOldFormat.newFileSize > 0);
-            assert.ok(result.offenders.imageOldFormat.newFileSize < result.offenders.imageOldFormat.fileSize);
+            ok(result.offenders.imageOldFormat.currentFormat, files.format);
+            strictEqual(result.offenders.imageOldFormat.fileSize, image.length);
+            ok(result.offenders.imageOldFormat.newFileSize > 0);
+            ok(result.offenders.imageOldFormat.newFileSize < result.offenders.imageOldFormat.fileSize);
         });
     });
 });
